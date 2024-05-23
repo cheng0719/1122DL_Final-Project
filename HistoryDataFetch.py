32d135ce812887339212a36ccec129bfd0452a4b
henrycheng227 <rosehj773@gmail.com>
2024-05-23 17:18:15 +0800

    Progress update 2023-05-23

--- HistoryDataFetch.py
+++ HistoryDataFetch.py
@@ -2,20 +2,21 @@ import yfinance as yf
 import pandas as pd
 
 # 使用YFinance抓取台積電股票資料
-stock_data = yf.download('2330.TW', start='2010-01-01', end='2023-12-31')
+stock_data = yf.download('2330.TW', start='2010-01-01', end='2018-12-31')
 
 # 取出所需欄位並重新命名欄位名稱
 stock_data = stock_data.reset_index()
 # stock_data = stock_data[['Date', 'High', 'Low', 'Open', 'Close']]
 # stock_data.columns = ['date', 'high', 'low', 'open', 'close']
-stock_data = stock_data[['Date', 'Open']]
-stock_data.columns = ['date', 'open']
+stock_data = stock_data[['Date', 'Close']]
+stock_data.columns = ['date', 'close']
 
 # 將數值的最小位設為整數個位數
-# stock_data['high'] = stock_data['high'].astype(int)
-# stock_data['low'] = stock_data['low'].astype(int)
-stock_data['open'] = stock_data['open'].astype(int)
-# stock_data['close'] = stock_data['close'].astype(int)
+# stock_data['high'] = stock_data['high']
+# stock_data['low'] = stock_data['low']
+# stock_data['open'] = stock_data['open']
+stock_data['close'] = stock_data['close']
+# .astype(int)
 
 # 轉換日期格式為'%Y%m%d'
 stock_data['date'] = stock_data['date'].dt.strftime("%Y-%m-%d %H:%M:%S")
@@ -24,4 +25,4 @@ stock_data['date'] = stock_data['date'].dt.strftime("%Y-%m-%d %H:%M:%S")
 stock_data = stock_data.sort_values(by='date')
 
 # 將資料輸出至CSV檔案
-stock_data.to_csv('./tsmc_stock_prices_INT_open_only_datetime_2010-2023.csv', index=False)
+stock_data.to_csv('./tsmc_prices_close_only_datetime_2010-2018.csv', index=False)
